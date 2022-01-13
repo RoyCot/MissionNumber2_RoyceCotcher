9a6f7e4cc0ab4e738b929a3d849a422b32784b0a
Royce Cotcher <RoyCot@users.noreply.github.com>
2022-01-12 21:11:27 -0700

    Added comments for grading

--- MissionNumber2_RoyceCotcher/wwwroot/js/gradeCalculator.js
+++ MissionNumber2_RoyceCotcher/wwwroot/js/gradeCalculator.js
@@ -1,16 +1,17 @@
 ﻿$("#btnSubmit").click(function () {
+    //Assign each input field's value to a variable
     let iAssign = $("#intAssign").val();
     let iGroup = $("#intGroup").val();
     let iQuiz = $("#intQuiz").val();
     let iExam = $("#intExam").val();
     let iIntex = $("#intIntex").val();
 
-
+    //Add up total score based on grade weights to one variable
     let iTotalScore = (iAssign * 0.55) + (iGroup * 0.05) +
         (iQuiz * 0.1) + (iExam * 0.2) + (iIntex * 0.1);
 
 
-
+    //If, else if, and else statements to print different options based on student's result
     if (iTotalScore > 89) {
         alert("Your score was " + iTotalScore + ". You got an A in the class");
     }
